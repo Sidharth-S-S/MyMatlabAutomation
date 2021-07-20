c25e83ff167bc9eb6e0dd28a410ed126a49500ae
Sidhartha S. S <33795979+mail2sidh@users.noreply.github.com>
2021-07-20 13:57:50 +0530

    Create MySIMFunctionCall.bat

---
+++ Scripts/MySIMFunctionCall.bat
@@ -0,0 +1,3 @@
+::  Integrate_SIM_Instance -t dadc -n DADC_10_6_2_2 -p D:\Integration\vmc_simulink_aos\Deliverables\Sim_Work.zip
+::  Integrate_SIM_Instance -j D:\customer_project\vmcsim.jlr\src_cm4sl\IntegrationDetailsDADC.json
+Integrate_SIM_Instance -t lmc -n LMC_CSW8_localdev_Part2_10 -p "D:\Artifacts_For_Integration\LMC OMCL\MLA_High_CSW8\Sim_Work_Sfun4VMCOnlySimMain.zip"
